d99f3d6b1b1303126297805d530577d1396e641e
arjunchakri <arjunchakri@gmail.com>
2017-03-25 18:07:21 +0530

    Add files via upload

--- pacman.java
+++ pacman.java
@@ -2,42 +2,55 @@
 import java.awt.*;
 import java.awt.event.KeyAdapter;
 import java.awt.event.KeyEvent;
+import java.util.Random;
 //import java.awt.event.*;
 import javax.swing.*;
 
 public class pacman extends JPanel {
 
     int x = 250, y = 250, flag3 = 0;
+    int xg1 = 100 , yg1 = 100;
+
     int[][] pointposition = new int[20][20];
     boolean pac_kill=false;
     int score=0;
     private static class Main1 {
 
-        static int flag = 1;
-        static int flag1 = 1;
+        static int flag3 = 1;  //ghost temp movement
+        static int flagg1 = 1; //ghost actual movement
+        static int flag = 1;//pac temp movement
+        static int flag1 = 1;//pac actual movement
     }
-
-    @Override
+@Override
     public void paint(Graphics g) {
         super.paint(g);
         if(pac_kill){
             System.out.println("game over");
+            int confirm = JOptionPane.showConfirmDialog(null, "Do you want to Play again?");
+            if(confirm==0){
+                pacman pc = new pacman();
+                
+            }
+            else if(confirm==1){
+                System.exit(0);
+            }
         }
         Graphics2D g2 = (Graphics2D) g;
-        Image background = Toolkit.getDefaultToolkit().getImage("D:\\Exps\\Js\\pacman\\src\\pacman\\gridbg.png");
-        Image pac1 = Toolkit.getDefaultToolkit().getImage("D:\\Exps\\Js\\pacman\\src\\pacman\\pac1.gif");
-        Image pac2 = Toolkit.getDefaultToolkit().getImage("D:\\Exps\\Js\\pacman\\src\\pacman\\pac2.gif");
-        Image pac3 = Toolkit.getDefaultToolkit().getImage("D:\\Exps\\Js\\pacman\\src\\pacman\\pac3.gif");
-        Image pac4 = Toolkit.getDefaultToolkit().getImage("D:\\Exps\\Js\\pacman\\src\\pacman\\pac4.gif");
 
-        Image ghost1 = Toolkit.getDefaultToolkit().getImage("D:\\Exps\\Js\\pacman\\src\\pacman\\ghost.png");
+        Image background = Toolkit.getDefaultToolkit().getImage("F:\\Softwares\\Java Proj\\pacman_java-master\\gridbg.png");
+        Image pac1 = Toolkit.getDefaultToolkit().getImage("F:\\Softwares\\Java Proj\\pacman_java-master\\pac1.gif");
+        Image pac2 = Toolkit.getDefaultToolkit().getImage("F:\\Softwares\\Java Proj\\pacman_java-master\\pac2.gif");
+        Image pac3 = Toolkit.getDefaultToolkit().getImage("F:\\Softwares\\Java Proj\\pacman_java-master\\pac3.gif");
+        Image pac4 = Toolkit.getDefaultToolkit().getImage("F:\\Softwares\\Java Proj\\pacman_java-master\\pac4.gif");
 
-        Image point = Toolkit.getDefaultToolkit().getImage("D:\\Exps\\Js\\pacman\\src\\pacman\\point.png");
+        Image ghost1 = Toolkit.getDefaultToolkit().getImage("F:\\Softwares\\Java Proj\\pacman_java-master\\ghost.png");
 
-        Image top = Toolkit.getDefaultToolkit().getImage("D:\\Exps\\Js\\pacman\\src\\pacman\\bordertop.png");
-        Image down = Toolkit.getDefaultToolkit().getImage("D:\\Exps\\Js\\pacman\\src\\pacman\\borderdown.png");
-        Image left = Toolkit.getDefaultToolkit().getImage("D:\\Exps\\Js\\pacman\\src\\pacman\\borderleft.png");
-        Image right = Toolkit.getDefaultToolkit().getImage("D:\\Exps\\Js\\pacman\\src\\pacman\\borderright.png");
+        Image point = Toolkit.getDefaultToolkit().getImage("F:\\Softwares\\Java Proj\\pacman_java-master\\point.png");
+
+        Image top = Toolkit.getDefaultToolkit().getImage("F:\\Softwares\\Java Proj\\pacman_java-master\\bordertop.png");
+        Image down = Toolkit.getDefaultToolkit().getImage("F:\\Softwares\\Java Proj\\pacman_java-master\\borderdown.png");
+        Image left = Toolkit.getDefaultToolkit().getImage("F:\\Softwares\\Java Proj\\pacman_java-master\\borderleft.png");
+        Image right = Toolkit.getDefaultToolkit().getImage("F:\\Softwares\\Java Proj\\pacman_java-master\\borderright.png");
 
         g2.drawImage(background, -5, -5, null);
         //pac movement
@@ -70,7 +83,7 @@ public class pacman extends JPanel {
                 break;
         }
         try {
-            Thread.sleep(10);
+            Thread.sleep(7);
         } catch (InterruptedException e) {
         }
 
@@ -100,8 +113,62 @@ public class pacman extends JPanel {
         }
         
         //--------------ghost
-        g2.drawImage(ghost1, 100, 100,null);
-        if(x/50==1 && y/50==1){
+    
+        Random r = new Random();
+int alter = r.nextInt(3);
+//Main1.flag3 = r.nextInt(4);
+        if(alter == 1){
+        if(x>xg1){
+                Main1.flag3 = 1;
+            
+        }
+        else{
+                Main1.flag3 = 3;
+        }
+        }
+        else if(alter == 2){
+        if(y>yg1){
+                Main1.flag3 = 4;
+            }
+            else{
+                Main1.flag3 = 2;
+            }
+        }
+        if(alter == 3){
+            Main1.flag3 = r.nextInt(4);
+        }
+        if (xg1 >= 450) {
+            xg1 = 0;
+        } else if (yg1 >= 450) {
+            yg1 = 0;
+        } else if (xg1 <= 0) {
+            xg1 = 450;
+        } else if (yg1 <= 0) {
+            yg1 = 450;
+        } else {
+            if (xg1 % 50 == 0 && yg1 % 50 == 0 && Main1.flag3 != Main1.flagg1) {
+                Main1.flagg1 = Main1.flag3;
+            }
+        }
+        
+        switch (Main1.flagg1) {
+            case 1:
+                g2.drawImage(ghost1, xg1++, yg1, null);
+                break;
+            case 2:
+                g2.drawImage(ghost1, xg1, yg1--, null);
+                break;
+            case 3:
+                g2.drawImage(ghost1, xg1--, yg1, null);
+                break;
+            case 4:
+                g2.drawImage(ghost1, xg1, yg1++, null);
+                break;
+        }
+        
+        
+        
+        if(x==xg1 && y==yg1){
             pac_kill=true;
         }
         g2.setFont(new Font("Tahoma",Font.BOLD,30));
@@ -112,14 +179,13 @@ public class pacman extends JPanel {
         
         
         repaint();
-
     }
 
-    public static void main(String[] args) {
+    public pacman(){
         JFrame f = new JFrame("pac");
         f.setSize(600, 600);
         //adds
-        f.add(new pacman(), BorderLayout.CENTER);
+        f.add(this, BorderLayout.CENTER);
         f.addKeyListener(new KeyAdapter() {
             @Override
             public void keyPressed(KeyEvent e) {
@@ -148,5 +214,8 @@ public class pacman extends JPanel {
         });
         f.setVisible(true);
         f.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+    }
+    public static void main(String[] args) {
+        new pacman();
     }
 }
